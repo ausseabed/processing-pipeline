6241837ba2c02de10cb9cebbf3d32a6512e2d3eb
arnab <arnab@raxit.me>
2019-10-28 01:21:25 +0000

    updates python caris wrapper to use pk from env var

--- myCarisBatch.py
+++ myCarisBatch.py
@@ -9,12 +9,13 @@ if len(sys.argv) < 2:
 hostname = sys.argv[1]
 command = sys.argv[2]
 password = sys.argv[3]
-key_filename = sys.argv[4]
+key_env_var_name = sys.argv[4]
 
 username = "Administrator"
 port = 22
 
 key_string = """q
+key_string = os.environ[key_env_var_name]
 """ # I saved my key in this string
 not_really_a_file = io.StringIO(key_string)
 private_key = paramiko.RSAKey.from_private_key(not_really_a_file,password=password)

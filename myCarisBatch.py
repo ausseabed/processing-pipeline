c0f9812723f115b6d26a978be5ae6166c28139b4
arnab <arnab@raxit.me>
2019-10-28 03:56:35 +0000

    corrects string enclosure

--- myCarisBatch.py
+++ myCarisBatch.py
@@ -14,9 +14,9 @@ print(key_env_var_name)
 username = "Administrator"
 port = 22
 
-key_string = """q
+key_string = """q""" # I saved my key in this string
 key_string = os.environ[key_env_var_name]
-""" # I saved my key in this string
+
 not_really_a_file = io.StringIO(key_string)
 print(key_string)
 private_key = paramiko.RSAKey.from_private_key(not_really_a_file,password=password)

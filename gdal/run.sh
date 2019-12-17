797dd59b8363cc38474a291dd40b9d904f1469c1
DaveP <ubuntu@ip-172-31-1-88.ap-southeast-2.compute.internal>
2019-12-17 03:59:48 +0000

    NGA-159 use env variable to ref tif output

--- gdal/run.sh
+++ gdal/run.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 echo Starting script
 env
-echo Starting translate
-#gdal_translate -co compress=lzw -b 1 -ot byte -scale 1 1 /vsis3/bathymetry-survey-288871573946-beagle-grid0/GA-0364_BlueFin_MB/BlueFin_2018-172_1m_coloured.tif output.tif 
-gdal_translate -co compress=lzw -b 1 -ot byte -scale 1 1 /vsis3/bathymetry-survey-288871573946/TestObject.tif output.tif 
+echo Starting translate of "$S3_SRC_TIF"
+VSIS3=`echo "$S3_SRC_TIF" | sed "s/^s3../vsis3/"`
+LOCALNAME=`echo "$VSIS3" | sed "s/.*\///" | sed "s/\.[^\.]\+$//"`
+S3DIR=`echo "$S3_SRC_TIF" | sed "s/\/[^\/]\+$/\//"`
+
+echo resulting variable VSIS3="$VSIS3"
+echo resulting variable LOCALNAME="$LOCALNAME"
+echo resulting variable S3DIR="$S3DIR"
+
+gdal_translate -co compress=lzw -b 1 -ot byte -scale 1 1 "$VSIS3" "$LOCALNAME".tif 
+#gdal_translate -co compress=lzw -b 1 -ot byte -scale 1 1 /vsis3/bathymetry-survey-288871573946/TestObject.tif output.tif 
 echo Starting Polygonise
-/usr/bin/gdal_polygonize.py output.tif output.shp 
+/usr/bin/gdal_polygonize.py "$LOCALNAME".tif "$LOCALNAME".shp
 echo AWS commit
-/usr/local/bin/aws2 s3 cp . s3://bathymetry-survey-288871573946-beagle-grid0/GA-0364_BlueFin_MB/ --recursive --include "output*"  --exclude "*" 
+/usr/local/bin/aws2 s3 cp . "$S3DIR" --recursive --include "$LOCALNAME"*  --exclude "*" 

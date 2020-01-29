70918b7e79172f8437846533c3be75d5594aac20
DaveP <ubuntu@ip-172-31-37-8.ap-southeast-2.compute.internal>
2020-01-29 01:38:10 +0000

    NGA-153 changing load balancer protocol

--- infra_warehouse/networking/main.tf
+++ infra_warehouse/networking/main.tf
@@ -128,7 +128,7 @@ resource "aws_lb_target_group" "geoserver_outside" {
 resource "aws_lb_listener" "geoserver_load_balancer_listener" {
   load_balancer_arn = "${aws_lb.geoserver_load_balancer.arn}"
   port              = "80"
-  protocol          = "HTTP"
+  protocol          = "TCP"
   default_action {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.geoserver_outside.arn}"

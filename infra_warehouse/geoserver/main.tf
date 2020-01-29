d0ef986888d944a1cecf2c24f17c2727ab1ad71c
DaveP <ubuntu@ip-172-31-37-8.ap-southeast-2.compute.internal>
2020-01-29 00:34:36 +0000

    NGA-153 adding elastic ip address to geoserver

--- infra_warehouse/geoserver/main.tf
+++ infra_warehouse/geoserver/main.tf
@@ -62,12 +62,55 @@ DEFINITION
 }
 
 
+resource "aws_eip" "geoserver_eip" {
+  count = 1
+  vpc   = true
+}
+resource "aws_lb" "geoserver_load_balancer" {
+  name               = "geoserver_load_balancer"
+  internal           = false
+  load_balancer_type = "network"
+  subnet_mapping {
+    subnet_id = "${var.public_subnets[0]}"
+    allocation_id = "${aws_eip.geoserver_eip.id}"
+  }
+
+  tags = {
+    Environment = "nonproduction"
+  }
+}
+
+resource "aws_lb_target_group" "geoserver_outside" {
+  name     = "geoserver_outside"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.tf_vpc.id}"
+  target_type = "ip"
+}
+
+
+resource "aws_lb_listener" "geoserver_load_balancer_listener" {
+  load_balancer_arn = "${aws_lb.geoserver_load_balancer.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.geoserver_outside.arn}"
+  }
+}
+
 resource "aws_ecs_service" "geoserver_service" {
   name            = "geoserver_service"
   cluster         = "${aws_ecs_cluster.geoserver_cluster.id}"
   task_definition = "${aws_ecs_task_definition.geoserver.arn}"
   desired_count   = 1
   launch_type = "FARGATE"
+  
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.geoserver_outside.arn}"
+    container_name   = "geoserver-task"
+    container_port   = 8080
+  }
 
   network_configuration {
     subnets="${var.public_subnets}"
@@ -76,4 +119,3 @@ resource "aws_ecs_service" "geoserver_service" {
   }
 
 }
-

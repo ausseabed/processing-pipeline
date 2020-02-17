23252b8b6adf38f8fe5eb6c69fce24e598ee081c
DaveP <ubuntu@ip-172-31-37-8.ap-southeast-2.compute.internal>
2020-02-17 00:54:56 +0000

    NGA-231 - Switch to templating of step function tf

--- infra/pipelines/main.tf
+++ infra/pipelines/main.tf
@@ -1,6 +1,16 @@
+locals {
+  pipeline_vars = { 
+    "var.ausseabed_sm_role" = "var.ausseabed_sm_role"
+    "var.aws_ecs_cluster_arn" = "var.aws_ecs_cluster_arn"
+    "var.aws_ecs_task_definition_gdal_arn" = "var.aws_ecs_task_definition_gdal_arn"
+    "var.aws_ecs_task_definition_caris_sg" = "var.aws_ecs_task_definition_caris_sg"
+    "var.aws_ecs_task_definition_caris_subnet" = "var.aws_ecs_task_definition_caris_subnet"
+  }
+}
+
 resource "aws_sfn_state_machine" "ausseabed-processing-pipeline-l3" {
   name     = "ausseabed-processing-pipeline-l3"
   role_arn = "${var.ausseabed_sm_role}"
   
-  definition = file("${path.module}/process_L3.json")
+  definition = templatefile("process_L3.json",local.pipeline_vars)
 }

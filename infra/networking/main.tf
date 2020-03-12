5a24a92f3c1be10aa227d5b3734f607643f47781
DaveP <david.penton@ga.gov.au>
2020-03-12 22:19:30 +0000

    NGA-211 tidy names for threading through firewall

--- infra/networking/main.tf
+++ infra/networking/main.tf
@@ -38,7 +38,7 @@ resource "aws_default_route_table" "default_route_table" {
   default_route_table_id = aws_vpc.tf_vpc.default_route_table_id
 
   tags = {
-    Name = "tf_private"
+    Name = "tf_private_def"
   }
 }
 
@@ -97,7 +97,7 @@ resource "aws_route_table" "tf_private_rt" {
 
 
   tags = {
-    Name = "tf_private"
+    Name = "tf_private_rt"
   }
 }
 
--- infra_warehouse/networking/outputs.tf
+++ infra_warehouse/networking/outputs.tf
@@ -1,18 +1,18 @@
 #-----networking/outputs.tf
 
 output "public_subnets" {
-  value = "${aws_subnet.tf_public_subnet.*.id}"
+  value = "${aws_subnet.warehouse_public_subnet.*.id}"
 }
 
 
 
 output "public_sg" {
-  value = "${aws_security_group.tf_public_sg.id}"
+  value = "${aws_security_group.warehouse_public_sg.id}"
 }
 
 
 output "subnet_ips" {
-  value = "${aws_subnet.tf_public_subnet.*.cidr_block}"
+  value = "${aws_subnet.warehouse_public_subnet.*.cidr_block}"
 }
 
 output "aws_ecs_lb_target_group_geoserver_arn"{

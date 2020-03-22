deed2caf26d2153635cc235f65b4459cde183630
DaveP <david.penton@ga.gov.au>
2020-03-22 22:22:18 +0000

    NGA-234 In ausseabed/warehouse-ogc-webservices

--- infra_warehouse/ancillary/outputs.tf
+++
@@ -1,7 +0,0 @@
-
-
-
-output "ecs_task_execution_role_svc_arn" {
-  value = "${aws_iam_role.ecs_task_execution_role_svc.arn}"
-}
-
--- infra_warehouse/networking/variables.tf
+++
@@ -1,9 +0,0 @@
-#------networking/variables.tf
-
-variable "vpc_cidr" {}
-
-variable "public_cidrs" {
-  type = list(string)
-}
-
-variable "accessip" {}

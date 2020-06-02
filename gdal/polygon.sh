3805014195e4f077a8fa91257c82b46c2a5debc1
DaveP <david.penton@ga.gov.au>
2020-06-02 06:09:40 +0000

    NGA-330 add destination param to polygon sig

--- gdal/polygon.sh
+++ gdal/polygon.sh
@@ -6,17 +6,25 @@ env
 
 # test with s3://bathymetry-survey-288871573946/TestObject.tif
 echo Starting translate of "$S3_SRC_TIF"
-VSIS3=`echo "$S3_SRC_TIF" | sed "s/^s3../\/vsis3/"`
-LOCALNAME=`echo "$VSIS3" | sed "s/.*\///" | sed "s/\.[^\.]\+$//"`
+VSIS3_SRC=`echo "$S3_SRC_TIF" | sed "s/^s3../\/vsis3/"`
+LOCALNAME=`echo "$VSIS3_SRC" | sed "s/.*\///" | sed "s/\.[^\.]\+$//"`
 S3DIR=`echo "$S3_SRC_TIF" | sed "s/\/[^\/]\+$/\//"`
 
-echo resulting variable VSIS3="$VSIS3"
+echo resulting variable VSIS3_SRC="$VSIS3_SRC"
 echo resulting variable LOCALNAME="$LOCALNAME"
 echo resulting variable S3DIR="$S3DIR"
 
-gdal_translate -co compress=lzw -b 1 -ot byte -scale 1 1 "$VSIS3" "$LOCALNAME".tif 
+echo To produce "$S3_DEST_SHP"
+VSIS3_DEST=`echo "$S3_DEST_SHP" | sed "s/^s3../\/vsis3/"`
+LOCALNAME_DEST=`echo "$VSIS3_DEST" | sed "s/.*\///" | sed "s/\.[^\.]\+$//"`
+S3DIR_DEST=`echo "$S3_DEST_SHP" | sed "s/\/[^\/]\+$/\//"`
+echo resulting variable VSIS3_DEST="$VSIS3_DEST"
+echo resulting variable LOCALNAME_DEST="$LOCALNAME_DEST"
+echo resulting variable S3DIR_DEST="$S3DIR_DEST"
+
+gdal_translate -co compress=lzw -b 1 -ot byte -scale 1 1 "$VSIS3_SRC" "$LOCALNAME_DEST".tif 
 #gdal_translate -co compress=lzw -b 1 -ot byte -scale 1 1 /vsis3/bathymetry-survey-288871573946/TestObject.tif output.tif 
 echo Starting Polygonise
-/usr/bin/gdal_polygonize.py "$LOCALNAME".tif "$LOCALNAME".shp
+/usr/bin/gdal_polygonize.py "$LOCALNAME_DEST".tif "$LOCALNAME_DEST".shp
 echo AWS commit
-/usr/local/bin/aws s3 cp . "$S3DIR" --recursive --exclude "*" --include "$LOCALNAME*" --exclude "*.tif" 
+/usr/local/bin/aws s3 cp . "$S3DIR_DEST" --recursive --exclude "*" --include "$LOCALNAME_DEST*" --exclude "*.tif" 

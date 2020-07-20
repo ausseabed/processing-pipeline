a33e8654e8380cc08500ead53718e6114ec112cb
DaveP <david.penton@ga.gov.au>
2020-07-20 05:01:31 +0000

    NGA-383: Compression level & download for hillshade

--- gdal/overlays.sh
+++ gdal/overlays.sh
@@ -40,7 +40,7 @@ echo Adding overlays
 gdaladdo -r average "$LOCALNAME_DEST"_in.tif 2 4 8 16
 
 echo Ensuring cloud optimised layout
-gdal_translate -co compress=DEFLATE -co TILED=YES -co BIGTIFF=IF_SAFER -co COPY_SRC_OVERVIEWS=YES -of COG "$LOCALNAME_DEST"_in.tif "$LOCALNAME_DEST".tif 
+gdal_translate -co compress=DEFLATE -LEVEL=9 -co TILED=YES -co BIGTIFF=IF_SAFER -co COPY_SRC_OVERVIEWS=YES -of COG "$LOCALNAME_DEST"_in.tif "$LOCALNAME_DEST".tif 
 
 echo AWS commit
 /usr/local/bin/aws s3 cp "$LOCALNAME_DEST".tif "$S3DIR_DEST""$LOCALNAME_DEST.tif"

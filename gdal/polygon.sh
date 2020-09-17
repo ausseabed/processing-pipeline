1f9ca411b800c83b8bad7f58fd8b03cedd39a7f3
DaveP <david.penton@ga.gov.au>
2020-09-17 23:57:43 +0000

    NGA-401: Expand+shrink for fast union + less self intersect

--- gdal/polygon.sh
+++ gdal/polygon.sh
@@ -30,25 +30,36 @@ then
   exit 0
 fi
 
-gdal_translate -co compress=DEFLATE -b 1 -ot byte -scale 1 1 1 1 "$VSIS3_SRC" "$LOCALNAME_DEST".tif 
-#gdal_translate -co compress=lzw -b 1 -ot byte -scale 1 1 /vsis3/bathymetry-survey-288871573946/TestObject.tif output.tif 
-echo Starting polygonise
-/usr/bin/gdal_polygonize.py "$LOCALNAME_DEST".tif polies.shp 
+echo "Create raster mask (1 across whole region)"
+gdal_translate -co compress=DEFLATE -b 1 -ot byte -scale 1 1 1 1 "$VSIS3_SRC" "$LOCALNAME_DEST"_a.tif 
 
-echo Creating single polygon exactly replicating raster
-ogr2ogr poly.shp polies.shp -dialect sqlite -sql "SELECT ST_Collect(geometry) AS geometry FROM polies"
+echo "Growing by a few pixels"
+/usr/bin/gdal_fillnodata.py -md 3 "$LOCALNAME_DEST"_a.tif "$LOCALNAME_DEST".tif -co compress=DEFLATE  
 
-# TODO change geographic into a multiplier
+echo "Identifying the cells one shy of the current edge"
+# Create an output tif because the NoData value is inherited from the output
+cp "$LOCALNAME_DEST".tif "$LOCALNAME_DEST"_prox.tif
+/usr/bin/gdal_proximity.py "$LOCALNAME_DEST"_prox.tif "$LOCALNAME_DEST"_prox.tif -distunits PIXEL -values 1 -maxdist 1 -co compress=DEFLATE 
 
-echo Adding area calcs to single polygon exactly replicating raster
+echo "Highlight the internal pixels (with value 1)"
+# Create an output tif because the NoData value is inherited from the output
+cp "$LOCALNAME_DEST"_prox.tif "$LOCALNAME_DEST"_prox2.tif
+/usr/bin/gdal_proximity.py "$LOCALNAME_DEST"_prox2.tif "$LOCALNAME_DEST"_prox2.tif -distunits PIXEL -values 1 -maxdist 3 -fixed-buf-val 0 -co compress=DEFLATE -use_input_nodata YES
+
+echo "Combine the rasters to form a mask"
+/usr/bin/gdal_merge.py -o "$LOCALNAME_DEST"_prox3.tif -n 255 -a_nodata 0 -co compress=DEFLATE "$LOCALNAME_DEST".tif "$LOCALNAME_DEST"_prox2.tif
+
+echo "Starting polygonise"
+/usr/bin/gdal_polygonize.py -8 "$LOCALNAME_DEST".tif -mask "$LOCALNAME_DEST"_prox3.tif  polies.shp 
+
+# calculating scaling factor
 if [ -z "${SCALING_FACTOR}" ] 
 then
   SCALING_FACTOR_SQ=1
 else
   SCALING_FACTOR_SQ=`echo "$SCALING_FACTOR * $SCALING_FACTOR" | bc`
 fi
-
-ogr2ogr "$LOCALNAME_DEST"_full.shp poly.shp -sql "SELECT *, CAST(OGR_GEOM_AREA * $SCALING_FACTOR_SQ / 1000000 AS float(19)) AS area_km2 FROM poly"
+echo SCALING_FACTOR_SQ = $SCALING_FACTOR_SQ
 
 gdalinfo "$LOCALNAME_DEST".tif 
 
@@ -61,16 +72,26 @@ fi
 
 SIMPLE_AREA=`echo "$SIMPLIFY_CELL_SIZE * $SIMPLIFY_CELL_SIZE" | bc`
 
-echo Creating single polygon
-ogr2ogr poly_min.shp polies.shp -dialect sqlite -sql "SELECT ST_Collect(geometry) AS geometry FROM polies WHERE Area(geometry) > $SIMPLE_AREA"
+echo "Creating single polygon"
+ogr2ogr poly_min.shp polies.shp -dialect sqlite -sql "SELECT ST_UNION(geometry) AS geometry FROM polies WHERE Area(geometry) > $SIMPLE_AREA"
 
-echo Adding area calcs
+echo "Adding area calcs"
 ogr2ogr area.shp poly_min.shp -sql "SELECT *, CAST(OGR_GEOM_AREA * $SCALING_FACTOR_SQ / 1000000 AS float(19)) AS area_km2 FROM poly_min"
 
-echo New cell size = $SIMPLIFY_CELL_SIZE
+echo "New cell size = $SIMPLIFY_CELL_SIZE"
 
-echo Simplifying polygon
+echo "Simplifying polygon"
 ogr2ogr "$LOCALNAME_DEST".shp area.shp -simplify $SIMPLIFY_CELL_SIZE
 
-echo AWS commit
+echo "Creating single polygon exactly replicating raster"
+ogr2ogr poly.shp polies.shp -dialect sqlite -sql "SELECT ST_Collect(geometry) AS geometry FROM polies"
+
+echo "Adding area calcs to single polygon exactly replicating raster"
+ogr2ogr "$LOCALNAME_DEST"_full.shp poly.shp -sql "SELECT *, CAST(OGR_GEOM_AREA * $SCALING_FACTOR_SQ / 1000000 AS float(19)) AS area_km2 FROM poly"
+
+echo "Copying area calcs from high res to low res"
+mv "$LOCALNAME_DEST".dbf "$LOCALNAME_DEST".dbf.bak
+cp "$LOCALNAME_DEST"_full.dbf "$LOCALNAME_DEST".dbf
+
+echo "AWS commit"
 /usr/local/bin/aws s3 cp . "$S3DIR_DEST" --recursive --exclude "*" --include "$LOCALNAME_DEST*" --exclude "*.tif" --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=id="$S3_ACCOUNT_CANONICAL_ID"
